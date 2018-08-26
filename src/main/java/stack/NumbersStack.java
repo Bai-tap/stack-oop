fffc2c2431475307e9723b430a9fa6f2d0a74ddf
longhoang94 <longsutls@gmail.com>
2018-08-26 10:53:05 +0700

    pass test-case testPush()

--- src/main/java/stack/NumbersStack.java
+++ src/main/java/stack/NumbersStack.java
@@ -1,11 +1,18 @@
 package stack;
 
 public class NumbersStack {
+    int count = 0;
+
     public NumbersStack() {
 
     }
 
     public int getCount() {
         return 0;
+    }
+
+    public int getPush(int number) {
+        count++;
+        return count;
     }
 }
