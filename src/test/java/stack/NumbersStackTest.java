80ea4479cb01caeffc536824844ea969156728d7
longhoang94 <longsutls@gmail.com>
2018-08-26 10:52:57 +0700

    add test-case testPush()

--- src/test/java/stack/NumbersStackTest.java
+++ src/test/java/stack/NumbersStackTest.java
@@ -1,10 +1,24 @@
 package stack;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NumbersStackTest {
-    public static void main(String[] args) {
+    @Test
+    public void testGetCount() {
         NumbersStack numbersStack = new NumbersStack();
-        if (numbersStack.getCount() == 0) {
-            System.out.println("success");
-        } else System.out.println("failed");
+        int expected = 0;
+        int actual = numbersStack.getCount();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPush() {
+        NumbersStack numbersStack = new NumbersStack();
+        int expected = 2;
+        int actual = numbersStack.getPush(10);
+        actual = numbersStack.getPush(20);
+        assertEquals(expected, actual);
     }
 }
